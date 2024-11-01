19c9559868b7d2b75c935f0431435763ff3238b8
jabir8210 <jabir8210@gmail.com>
2024-11-01 17:44:17 +0100

    Data færdig små rettelser Debugging

---
+++ Model/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+public class MenuItem : ITableEntity
+{
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public string VarmRet { get; set; } 
+    public string KoldRet { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+}
